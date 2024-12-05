092dccd75a9f6e24780ce99886a456e174086c29
Wiederlind <daniilf2013@mail.ru>
2024-12-05 20:58:08 +0500

    migrations

---
+++ database/migrations/2024_12_05_150609_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('tutor_id')->constrained();
+            $table->foreignId('subject_id')->constrained();
+            $table->string('status')->default('pending');
+            $table->timestamp('lesson_date');
+            $table->integer('duration');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
